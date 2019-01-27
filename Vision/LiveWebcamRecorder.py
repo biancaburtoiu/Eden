8df9f18a001fc1cae92e9de2f28436e1547ec7ce
Kieran Litschel <s1614973@middleton.inf.ed.ac.uk>
2019-01-27 14:28:34 +0000

    Added more functionality to webcam recorder

--- Vision/LiveWebcamRecorder.py
+++ Vision/LiveWebcamRecorder.py
@@ -1,4 +1,5 @@
 import cv2
+import math
 
 
 def set_res(cap, x, y):
@@ -7,21 +8,32 @@ def set_res(cap, x, y):
     return str(cap.get(cv2.CAP_PROP_FRAME_WIDTH)), str(cap.get(cv2.CAP_PROP_FRAME_HEIGHT))
 
 
-def show_webcam():
+def record_on_zero(path="record_output/", start_i=0):
+    cam = cv2.VideoCapture(0)
+    set_res(cam, 1920, 1080)
+    i = start_i
+    while True:
+        ret_val, img = cam.read()
+        cv2.imshow('my webcam', img)
+        if cv2.waitKey(1) == 48:
+            cv2.imwrite(path + "%s.jpg" % i, img)
+            i += 1
+
+
+def constant_record(path="record_output/", fps=2):
     cam = cv2.VideoCapture(0)
     set_res(cam, 1920, 1080)
     i = 0
     while True:
         ret_val, img = cam.read()
         cv2.imshow('my webcam', img)
-        if cv2.waitKey(1) == 48:
-            cv2.imwrite("%s.jpg" % i, img)
-            i += 1
-    cv2.destroyAllWindows()
+        if i % math.ceil(10 / fps) == 0:
+            cv2.imwrite(path + "%s.jpg" % i, img)
+        i += 1
 
 
 def main():
-    show_webcam()
+    record_on_zero(start_i=32)
 
 
 if __name__ == '__main__':
