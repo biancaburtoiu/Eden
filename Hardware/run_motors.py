5591a94646f0156c33f0c428f63c499b5b20aeaf
s1632620 <s1632620@ed.ac.uk>
2019-01-30 17:49:59 +0000

    refactored motors script

--- Hardware/run_motors.py
+++ Hardware/run_motors.py
@@ -1,21 +1,72 @@
 import ev3dev.ev3 as ev3
 import sys
 #motor = sys.argv[2]
-motors = []
 
-for i in range(1,len(sys.argv)):
-  motors.append(ev3.LargeMotor('out'+sys.argv[i]))
-
-while True:
-    inputs = input("e:").split(" ") 
-    if inputs[0]=='c' :
-        for m in motors:
-            m.run_forever(speed_sp=int(inputs[1]))
-    elif inputs[0]=='t':
-        for m in motors:
-            m.run_timed(speed_sp=int(inputs[1]), time_sp=int(inputs[2]))
-    elif inputs[0] =='s':
-        for m in motors:
-            m.run_timed(speed_sp=0, time_sp=0)
+def move(rotate,speed,time):
+    if speed>1050 or speed < -1050:
+        #speed must be in range [-1050,1050]
+        print("too fast")
+    elif time<0:
+        # flag for constant movement
+        if rotate:
+            motors[0].run_forever(speed_sp=speed)
+            motors[1].run_forever(speed_sp=-speed)
+        else:  
+            motors[0].run_forever(speed_sp=speed)
+            motors[1].run_forever(speed_sp=speed)
     else:
-        print("NOT WELL FORMED")
+        #timed movement
+        if rotate:
+            motors[0].run_timed(speed_sp=speed, time_sp=time)
+            motors[1].run_timed(speed_sp=-speed, time_sp=time)
+        else:
+            motors[0].run_timed(speed_sp=speed, time_sp=time)
+            motors[1].run_timed(speed_sp=speed, time_sp=time)
+
+
+def stop():
+    for m in motors:
+        m.run_timed(speed_sp=0, time_sp=0)
+
+#add two motors to array, from command line args
+#motors[0] is left, motors[1] is right
+
+motors = []
+def setupMotors():
+    for i in range(1,len(sys.argv)):
+    motors.append(ev3.LargeMotor('out'+sys.argv[i]))
+setupMotors()
+
+def debugfunc():
+    while True:
+        ##wait for input
+        inputs = input("e:").split(" ")
+
+        ##check input format, and perform action
+        if inputs[0]=='m':
+            if inputs[1]=='c' :
+                speed = int(inputs[2])
+                move(False,speed,-1)
+            elif inputs[1]=='t':
+                speed = int(inputs[2])
+                time = int(inputs[3])
+                move(False,speed,time)
+            else:
+                print("NOT WELL FORMED")
+        elif inputs[0]=='r':
+            if inputs[1]=='c' :
+                speed = int(inputs[2]) 
+                move(True,speed,-1)
+            elif inputs[1]=='t':
+                speed = int(inputs[2])
+                time = int(inputs[3])
+                move(True,speed,time)
+            else:
+                print("NOT WELL FORMED")
+        elif inputs[0] =='s':
+            stop()
+        else:
+            print("NOT WELL FORMED")
+
+if __name__ == "__main__":
+    debugfunc()
