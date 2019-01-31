c877fe4b07d51f1c45b85e1fc166767f9b8a1b7c
Finlay <finwashere@gmail.com>
2019-01-31 16:38:36 +0000

    further progress to A* algorithm

--- pathfinding/graph.py
+++ pathfinding/graph.py
@@ -1,52 +1,59 @@
 import queue as Q
+from math import sqrt
+
 
 class Graph:
 
     def __init__(self):
-        self.root = Node((0,0))
+        self.root = Node((0, 0))
 
-    def pathNotFound(self,openSet,goal):
-        lowestCostNode = openSet.get()
-        openSet.put(lowestCostNode)
-        return lowestCostNode.pos == goal.pos
-
-    def searchGraph(self,node1,node2):
-        #initially we have a priority queue of nodes, ordered by distance
-        #(assuming all distances are 1), and an empty result set
-        openSet = Q.PriorityQueue()
-        openSet.put(node1)
+    def searchGraph(self, start, goal):
+        # initially we have a priority queue of nodes, ordered by distance
+        # (assuming all distances are 1), and an empty result set
+        open_set = Q.PriorityQueue()
+        open_set.put((start, 0))
         closed = []
 
-        while self.pathNotFound(openSet,node2):
-            current = openSet.get(self)
-
-
-
-
+        while not open_set.empty():
+            current = open_set.get()
+            if current.pos == goal.pos:
+                None
+                # work out path
+            else:
+                closed.append(current)
+                for neighbour in current.getNeighbours():
+                    cost = neighbour.getCost() + 1  # (node distances are 1)
+                    if neighbour not in closed or cost < neighbour.getCost():
+                        neighbour.setCost(cost)
+                        open_set.put((neighbour,cost+sld(neighbour,goal)))
 
 
 class Node:
-    def __init__(self,pos):
-        # a list of neighbour nodes 
-        self.pos=pos
-        self.up=None
-        self.down=None
-        self.left=None
-        self.right=None
+    def __init__(self, pos):
+        self.pos = pos
 
-    def addUpNeighbour(self,n):
-        self.up=n
+        # (node,direction) pairs!
+        # direction should be l,r,u,d
+        self.neighbours = []
 
-    def addDownNeighbour(self,n):
-        self.down=n
-
-    def addLeftNeighbour(self,n):
-        self.left=n
-
-    def addRightNeighbour(self,n):
-        self.right=n
+        self.cost=0
 
     def getPos(self):
         return self.pos
 
+    def getNeighbours(self):
+        return self.neighbours
 
+    # neighbour argument should be a pair: (Node obj, direction)
+    def addNeighbour(self, n):
+        self.neighbours.append(n)
+
+    def getCost(self):
+        return self.cost
+
+    def setCost(self,c):
+        self.cost = c
+
+# calculate euclidean distance between two nodes
+def sld(node1,node2):
+    return sqrt(node1.getPos()^2 + node2.getPos()^2)
