c92365829b6f54b5074a67f7e942f37016acd115
s1632620 <s1632620@ed.ac.uk>
2019-02-01 18:36:46 +0000

    added method to turn graph into grid, and improved presentation of code

--- pathfinding/graph.py
+++ pathfinding/graph.py
@@ -1,12 +1,17 @@
 import queue as Q
 from math import sqrt
 
+
 # ================= Graph class: references root node, and provides operations==
 class Graph:
 
-    def __init__(self,pos=(0,0)):
+    def __init__(self, pos=(0, 0)):
         self.root = Node(pos)
 
+
+    # perform A* on this graph, starting at the start Node, and finding a path to
+    # the (x,y) pair given as goal_pos.
+    # it will return an ordered list of directions ('u','d','l','r')
     def searchGraph(self, goal_pos):
         # initially we have a priority queue of nodes:
         #   queue is ordered by cost so far + heuristic estimate to goal
@@ -14,39 +19,43 @@ class Graph:
         # and a map from node to node's parent, so we can work out the
         # best route from the start to a specific node
 
-        start = self.root # start from root of graph
-        open_set = Q.PriorityQueue()
-        open_set.put((0,start))
-        cost_so_far={}
+        start = self.root  # start from root of graph
+        open_set = Q.PriorityQueue() # description of this structure near of Node class
+        open_set.put((0, start))
+        cost_so_far = {}
         parent = {}
         cost_so_far[start] = 0
 
+        # continue until we've found all the nodes, unless the goal is reached
         while not open_set.empty():
             # take the lowest cost node first
-            (_,current) = open_set.get()
+            (_, current) = open_set.get()
+
             if current.getPos() == goal_pos:
                 # in this case, this is our goal
-                return self.constructPath(parent, current,start)
-                # work out path
+                return self.constructPath(parent, current, start)
 
             else:
                 # we must keep exploring
 
-                for neighbour,_ in current.getNeighbours():
+                for (neighbour, _) in current.getNeighbours():
                     cost = cost_so_far[current] + 1  # (node distances are 1)
                     if neighbour not in cost_so_far.keys() or cost < cost_so_far[neighbour]:
                         # we have found the best route to this node (so far)
                         cost_so_far[neighbour] = cost
-                        # note use of sld() as a measure of goal closeness
-                        cost_for_node = int(cost+mbd(neighbour,Node(goal_pos)))
-                        open_set.put( ( cost_for_node, neighbour ) )
+                        # note use of sld()/mbd() as a measure of goal closeness
+                        cost_for_node = int(cost + mbd(neighbour, Node(goal_pos)))
+                        open_set.put((cost_for_node, neighbour))
                         parent[neighbour] = current
 
-    def constructPath(self,parent_dict, goal,start):
+        # we didn't find the goal!
+        return None
+
+    def constructPath(self, parent_dict, goal, start):
         # start at the goal
         current = goal
         # this will be a list of nodes, in order
-        node_path=[current]
+        node_path = [current]
 
         # until we get to the start..
         while current != start:
@@ -55,28 +64,34 @@ class Graph:
             # tracking which nodes we pass through
             node_path.insert(0, current)
 
-
         dir_path = []
-        #now convert the path into a list of directions
-        for i in range(0,len(node_path)-1):
-            dir_path.append(node_path[i].dirToNeighbour(node_path[i+1]))
+        # now convert the path into a list of directions
+        for i in range(0, len(node_path) - 1):
+            dir_path.append(node_path[i].neighbourToDir(node_path[i + 1]))
 
-        return node_path,dir_path
+        return node_path, dir_path
 
     # takes a starting position pair, and a 2d bool grid of visitable squares
     # and turns it into a graph structure
-    def pathFromGrid(self,start_pos,grid):
+    # A grid square is 0 iff the square is accessible
+    def graphFromGrid(self, start_pos, grid):
         # sychronise root note and starting position
         self.root.setPos(start_pos)
 
         # we will have a stack of nodes to visit
         node_stack = [self.root]
 
-        # list of co ords, relative to us, that we want to visit
-        COORD_DIFFS = [((0,1),'u'),((-1,0),'l'),((0,-1),'d'),((1,0),'r')]
-        while (len(node_stack)):
+        # list of co ords, relative to us, that we want to visit. They are stored as
+        # ( (x-pos, y-pos) , direction) nested pairs
+        COORD_DIFFS = [((0, 1), 'u'), ((-1, 0), 'l'), ((0, -1), 'd'), ((1, 0), 'r')]
+
+
+
+
+        # continue until we've visited every node
+        while len(node_stack):
             current = node_stack.pop()
-            (x,y) = current.getPos()
+            (x, y) = current.getPos()
 
             # make the absolute co ords for the given current node
 
@@ -97,102 +112,166 @@ class Graph:
 
                     coords.append(new_element)
 
-                    procedural way'''
+                    end procedural way'''
 
             # list comprehension of above. Adds the absolute co ords to list
             # only if they are within the boundary of the grid
-            neighbour_coords = [((a+x,b+y),d) for ((a,b),d) in COORD_DIFFS
-                if x+a in range(0,len(grid[0])) and y+b in range(0,len(grid))]
+            neighbour_coords = [((a + x, b + y), d) for ((a, b), d) in COORD_DIFFS
+                                if x + a in range(0, len(grid[0])) and y + b in range(0, len(grid))]
 
-            #now loop through remaining coords - these are the neighbours in the grid
-            for ((x,y),d) in neighbour_coords:
-                if grid[y][x]==0:
+            # now loop through remaining coords - these are the neighbours in the grid
+            for ((x, y), d) in neighbour_coords:
+                if grid[y][x] == 0:
                     # an unvisited, clear square. make a node and add it to stack
-                    grid[y][x] = Node((x,y))
+                    grid[y][x] = Node((x, y))
                     node_stack.append(grid[y][x])
 
                     # note the ==0 case still falls through vv
-                if grid[y][x]!=1:
+                if grid[y][x] != 1:
                     # any square that isn't an obstacle
 
-                    grid[y][x].addNeighbour((invert(d),current))
-                    current.addNeighbour((d,grid[y][x]))
+                    # create mutual neighbouring relation
+                    grid[y][x].addNeighbour((invert(d), current))
+                    current.addNeighbour((d, grid[y][x]))
 
-# ============== Node class: should not be used individually, used by Graph to=
-# ==============represent a node===============================================
+                # anything that misses both ifs is an obstacle, ignore it!
+
+        # the grid is now in graph form. This Graph object references the node for start_pos
+
+    #for debugging, turns our graph into a grid.
+    #pass in the size of the grid, if it's too small this will probably crash
+    def gridFromGraph(self,size):
+        # create grid using list comprehension, assumes everything is obstacle initially
+        grid = [[1] * size[0] for i in range(0, size[1])]
+
+        node_stack = [self.root]
+        visited = []
+        while len(node_stack):
+            current = node_stack.pop()
+            visited.append(current)
+
+            (x,y) = current.getPos()
+            try:
+                grid[y][x] = 0
+            except:
+                None
+
+            for (n,_) in current.getNeighbours():
+                if n not in visited:
+                    node_stack.append(n)
+
+        return grid
+
+
+
+# ============== Node class: should not be instantiated individually, used by Graph to=
+# ==============represent a node and it's neighbours===================================
 class Node:
     def __init__(self, pos):
         # tuple co ordinate
         self.pos = pos
 
-        # dictionary mapping directions ('l','r','u','d') to nodes!
+        # dictionary mapping nodes to directions
         self.neighbours = {}
 
-
-    def __lt__(self,other):
+    # the priority queue contains tuples of (score,Node). If scores are equal, it tries to compare nodes.
+    # if the score is the same, we don't care. So we implement a trivial comparison function for python to use
+    def __lt__(self, other):
         return 1
 
     def __repr__(self):
-        return "(" + str(self.pos[0])+","+str(self.pos[1])+ ")"
+        return "(" + str(self.pos[0]) + "," + str(self.pos[1]) + ")"
 
     def __str__(self):
-        return "(" + str(self.pos[0])+","+str(self.pos[1])+ ")"
+        return "(" + str(self.pos[0]) + "," + str(self.pos[1]) + ")"
 
     def getPos(self):
         return self.pos
 
     def setPos(self, new_pos):
-        self.pos=new_pos
+        self.pos = new_pos
 
     def getNeighbours(self):
         return self.neighbours.items()
 
     # neighbour argument should be a pair: (Node obj, direction)
     def addNeighbour(self, n):
-        self.neighbours[n[1]]=n[0]
+        self.neighbours[n[1]] = n[0]
 
     # returns direction from this node to neighbour node
-
-    #####rename it
-    def dirToNeighbour(self,neighbour):
+    def neighbourToDir(self, neighbour):
         return self.neighbours[neighbour]
 
 
+# ==============Main program==================================================
+
+# =====heuristics====
+
 # calculate euclidean distance between two nodes
-def sld(node1,node2):
-    (x1,y1) = node1.getPos()
-    (x2,y2) = node2.getPos()
-    return sqrt((x1-x2)**2 + (y1-y2)**2)
+def sld(node1, node2):
+    (x1, y1) = node1.getPos()
+    (x2, y2) = node2.getPos()
+    return sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2)
+
 
 # calculate manhatton block distance between two nodes
-def mbd(node1,node2):
-    (x1,y1) = node1.getPos()
-    (x2,y2) = node2.getPos()
-    return abs(x1-x2) + abs(y1-y2)
+def mbd(node1, node2):
+    (x1, y1) = node1.getPos()
+    (x2, y2) = node2.getPos()
+    return abs(x1 - x2) + abs(y1 - y2)
 
+
+# ====helpers======
+
+# up is opposite of down, etc
 def invert(d):
-    if d=='d':
-         return 'u'
-    if d=='u':
-         return 'd'
-    if d=='l':
-         return 'r'
+    if d == 'd':
+        return 'u'
+    if d == 'u':
+        return 'd'
+    if d == 'l':
+        return 'r'
     else:
-         return 'l'
+        return 'l'
 
+# prints a grid
 def gridprint(grid):
-    grid.reverse()
+    gridcopy = grid.copy()
+    gridcopy.reverse()
     for row in grid:
         print(row)
 
+def plotPath(grid,path):
+    for node in path:
+        (x,y) = node.getPos()
+        grid[y][x] = 5
+    return grid
+# =====main=======
+
 def main():
-    grid = [[0,1,0],[0,0,0],[0,0,0]]
-    gridprint(grid.copy())
-    target = (2,0)
-    print("target: ",target)
+    # watch out because this grid is upside down ((0,0) top left
+    # but the co ordinate system works normally ((0,0) bottom left
+    # - the gridprint function will flip the grid for you
+
+    grid = [
+        [0, 0, 0, 0, 1, 0, 0, 1],
+        [0, 1, 0, 0, 1, 1, 0, 0],
+        [0, 0, 1, 0, 0, 0, 1, 0],
+        [0, 0, 1, 0, 1, 0, 1, 0],
+        [0, 0, 1, 0, 1, 1, 0, 0],
+        [0, 1, 0, 0, 0, 0, 1, 0],
+        [0, 0, 0, 0, 1, 0, 0, 0],
+        ]
+    gridprint(grid)
+    target = (6,0)
+    print("target: ", target)
     graph = Graph()
-    graph.pathFromGrid((0,0),grid)
-    print(graph.searchGraph(target))
+    graph.graphFromGrid((0, 0), grid)
+    path,dirs = graph.searchGraph(target)
+    gridprint(plotPath(graph.gridFromGraph((8,7)),path))
+    print(dirs)
+    print("length: ",len(path))
+
 
 if __name__ == "__main__":
     main()
