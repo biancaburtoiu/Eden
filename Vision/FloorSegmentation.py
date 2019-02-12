c99d7c6bd8856ec8cc7d35454a199eedad10d951
Yejunlang <2250758674@qq.com>
2019-02-12 18:10:10 +0000

    add background subtraction (may not needed)

--- Vision/FloorSegmentation.py
+++ Vision/FloorSegmentation.py
@@ -37,6 +37,39 @@ def segmentation(img):
     cv2.imshow('res2', res2)
     cv2.waitKey(0)
 
+def rgb_norm(img):
+
+    image = img.copy()
+    cv2.imshow("ms", image)
+    r = image[:, :, 0]
+    g = image[:, :, 1]
+    b = image[:, :, 2]
+    image[:,:,0] = (r / (r + g + b)) * 255
+    image[:,:,1] = (g / (r + g + b)) * 255
+    image[:,:,2] = (b / (r + g + b)) * 255
+    return image
+
+
+def backgroundSubtraction(back, fore):
+    # back = cv2.GaussianBlur(back, (5, 5), 0)
+    # fore = cv2.GaussianBlur(fore, (5, 5), 0)
+    back = cv2.pyrMeanShiftFiltering(back, 50, 20, maxLevel=2)
+    cv2.imshow("back", back)
+    fore = cv2.pyrMeanShiftFiltering(fore, 50, 20, maxLevel=2)
+    cv2.imshow("fore", fore)
+    thresh = 15
+
+    diff = cv2.absdiff(back, fore)
+    cv2.imshow('BACK', diff)
+    _, diffB = cv2.threshold(diff[:, :, 0], thresh, 255, cv2.THRESH_BINARY)
+    cv2.imshow('B', diffB)
+    _, diffG = cv2.threshold(diff[:, :, 1], thresh, 255, cv2.THRESH_BINARY)
+    cv2.imshow('G', diffG)
+    _, diffR = cv2.threshold(diff[:, :, 2], thresh, 255, cv2.THRESH_BINARY)
+    cv2.imshow('R', diffR)
+    return cv2.bitwise_or(diffB, cv2.bitwise_or(diffR, diffG))
+
+
 def floorDetection(img):
 
     gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
@@ -54,15 +87,14 @@ def floorDetection(img):
 
     blurred = cv2.GaussianBlur(img, (5, 5), 0)
     canny = cv2.Canny(blurred, 0, 255)
-    # cv2.imshow("edge", canny)
+    cv2.imshow("edge", canny)
     (_, thresh) = cv2.threshold(canny, 50, 255, cv2.THRESH_BINARY)
 
 
-    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (15, 15))
+    kernel = cv2.getStructuringElement(cv2.MORPH_ELLIPSE, (10, 10))
 
     closed = cv2.morphologyEx(thresh, cv2.MORPH_CLOSE, kernel)
-    closed = cv2.erode(closed, None, iterations=2)
-    # cv2.imshow("closed", closed)
+    closed = cv2.erode(closed, None, iterations=1)
 
     # Threshold in hsv space in order to deal with varying light.
     # Since the background is almost grey, the hue value is from 0 to 360(0 to 180 in opencv)
@@ -75,22 +107,17 @@ def floorDetection(img):
     # img = cv2.filter2D(img, -1, kernel)
     hsv = cv2.cvtColor(img, cv2.COLOR_RGB2HSV)
     lower = (0, 0, 40) # lower bound
-    higher = (180, 120, 200) # change if white is not acceptable, s can not be smaller than 50
+    higher = (180, 120, 220) # change if white is not acceptable, s can not be smaller than 50
 
     threshold2 = cv2.inRange(hsv, lower, higher)
     reverse = cv2.bitwise_not(threshold2)
-    # cv2.imshow("img", img)
-    # cv2.imshow("thresh1", reverse)
     reverse = cv2.erode(reverse, kernel=(5, 5), iterations=1)
     reverse = cv2.dilate(reverse, None, iterations=1)
-    # cv2.imshow("threshold", reverse)
 
-    reverse = cv2.bitwise_or(reverse, thresh)
-    reverse = cv2.dilate(reverse, kernel=(3,3), iterations=1)
-    reverse = cv2.erode(reverse, kernel=(3, 3), iterations=1)
-    # reverse = cv2.morphologyEx(reverse, cv2.MORPH_CLOSE, kernel)
-
-    # cv2.imshow("final", reverse)
+    reverse = cv2.bitwise_or(reverse, closed)
+    # reverse = cv2.dilate(reverse, kernel=(3,3), iterations=1)
+    # reverse = cv2.erode(reverse, kernel=(3, 3), iterations=1)
+    reverse = cv2.morphologyEx(reverse, cv2.MORPH_CLOSE, kernel)
 
     return reverse
 
