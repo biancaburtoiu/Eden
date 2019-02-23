209f02bcb6ff63f81ab456ad014b262acd207ea3
s1632620 <s1632620@ed.ac.uk>
2019-02-23 21:20:01 +0000

    starting vision based approach - client side code for ev3

--- Hardware/client_monitored_ver.py
+++ Hardware/client_monitored_ver.py
@@ -4,17 +4,23 @@ import sys
 import ev3dev.ev3 as ev3
 import math
 import time
+global currently_moving
+global client
+global movement_controller
 
 def onConnect(client,userdata,flags,rc):
     print("connected with result code %i" % rc)
-    client.subscribe("instructions")
-    client.subscribe("pos")
-    ev3.Sound.speak("connected")
+    client.subscribe("start-instruction")
+    client.publish("finish-instruction","0",qos=2)
+
+    ###garbage###
+    ev3.Sound.speak("BANG")
+    time.sleep(1)
+    ev3.Sound.speak("BANG")
+    #############
 
 def onMessage(client,userdata,msg):
     try:
-        print("Received message with payload:%s "%(msg.payload.decode()))
-
         if msg.topic=="start-instruction":
             # single instruction:
             # - face direction, start moving, stop moving, or rotate
@@ -29,6 +35,8 @@ def onMessage(client,userdata,msg):
 
 def follow_one_instruction(instruction_as_string):
     print("about to follow instruction: %s"%instruction_as_string)
+    global currently_moving
+
     if currently_moving:
         # stop (moving) instruction
         if instruction_as_string=='s':
@@ -41,14 +49,16 @@ def follow_one_instruction(instruction_as_string):
         # fresh instruction / not a stop instruction
 
         # absolute turn instructions ( first inst received)
-        if instruction_as_string=='u':
-            movement_controller.absolute_turn(0)
-        elif instruction_as_string=='r':
-            movement_controller.absolute_turn(90)
-        elif instruction_as_string=='d':
-            movement_controller.absolute_turn(180)
-        elif instruction_as_string=='l':
-            movement_controller.absolute_turn(270)
+        if instruction_as_string in ['u','d','l','r']:
+            if instruction_as_string=='u':
+                movement_controller.absolute_turn(0)
+            elif instruction_as_string=='r':
+                movement_controller.absolute_turn(90)
+            elif instruction_as_string=='d':
+                movement_controller.absolute_turn(180)
+            elif instruction_as_string=='l':
+                movement_controller.absolute_turn(270)
+            ask_for_next_inst(movement_controller.angle)
 
         # movement / relative turn instructions (subsequent insts)
         else:
@@ -56,31 +66,30 @@ def follow_one_instruction(instruction_as_string):
             # in form (r,[degrees]), or (m,[squares])
             # note the number of squares is unused for the main navigation
             (inst_type,inst_val) = tuple(instruction_as_string.split(","))
+            inst_val = int(inst_val)
             if inst_type=='m':
                 currently_moving=True
                 movement_controller.forward_forever()
             elif inst_type=='r':
-                currently_moving = True
                 movement_controller.relative_turn(inst_val)
                 ask_for_next_inst(movement_controller.angle)
             else:
                 print("received a malformed instruction!: %s"%instruction_as_string)
 
 def ask_for_next_inst(current_angle):
-    client.publish("finish-instruction",str(current_angle))
+    print("about to send finish-instruction ",current_angle)
+    client.publish("finish-instruction",str(current_angle),qos=2)
+    print("instruction sent!")
 
 # set up client & client functions
-global client
 client=mqtt.Client("ev3")
 client.on_connect=onConnect
 client.on_message=onMessage
 
 # if this is true, robot is waiting for a stop instruction from vision
-global currently_moving
 currently_moving = False
 
 # movement object, with initial angle of 0
-global movement_controller
 movement_controller = movement_monitored_ver.Movement(0)
 
 #connect client and make it wait for inputs
