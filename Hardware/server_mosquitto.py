21ae60024e5b30fb0955fb3fd898e41106a68b3a
KieranLitschel <kieran.litschel@hotmail.co.uk>
2019-03-31 15:24:37 +0100

    Made mosquitto server more functional

--- Hardware/server_mosquitto.py
+++ Hardware/server_mosquitto.py
@@ -2,23 +2,29 @@
 
 import paho.mqtt.client as mqtt
 
-def onConnect(client,userdata,flags,rc):
+
+def onConnect(client, userdata, flags, rc):
     print("connected with result code %i" % rc)
 
-client=mqtt.Client("desktopMachine")
 
-client.on_connect=onConnect
+client = mqtt.Client("desktopMachine")
+
+client.on_connect = onConnect
 client.connect("129.215.3.65")
 client.subscribe("navigate-finish")
 
-def on_message(cl,ud,fl):
+
+def on_message(cl, ud, fl):
     print("FINISHED UWU")
 
 
 client.loop_start()
+
+
 def remoteMove(insts_string):
     """send a command to the ev3 to move remotely"""
-    client.publish("instructions",insts_string)
+    client.publish("instructions", insts_string)
+
 
 def debugfunc():
     while True:
@@ -26,7 +32,11 @@ def debugfunc():
         inputi = input("e:").strip().split("\n")
 
         ##check input format, and perform action
-        client.publish("navigate-start",inputi[0].encode())
+        if inputi[0] == "local":
+            client.publish("close-navigate", "")
+        else:
+            client.publish("navigate-start", inputi[0].encode())
+
 
 if __name__ == "__main__":
     debugfunc()
